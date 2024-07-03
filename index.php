a66f5dae04bd7ae72085afc6caa10066dea7a8f9
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-07-04 01:09:41 +0600

    Completed image carousel using Swiper JS in main section

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web Application</title>
+    <!-- Font awesome CDN Link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <!-- Swiper JS CDN Link  -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -27,6 +30,48 @@
       <li class="menu-button" onclick=showSidebar()><a href="#"><i class="fa-solid fa-bars" style="color: #e45f07;"></i></a></li>
     </ul>
   </nav>
+  <main class="main">
+    <div class="container swiper">
+        <div class="slider-wrapper">
+            <div class="card-list swiper-wrapper">
+                <div class="card-item swiper-slide">
+                    <img class="user-image" src="assets/image_01.jpg" alt="Jack">
+                    <h2 class="user-name">Jack</h2>
+                    <p class="user-profession">Software Engineer</p>
+                    <button class="message-button">Contact With me</button>
+                </div>
+                <div class="card-item swiper-slide">
+                    <img class="user-image" src="assets/images_02.jpg" alt="Jack">
+                    <h2 class="user-name">Mr. Umar</h2>
+                    <p class="user-profession">Machine Learning Engineer</p>
+                    <button class="message-button">Contact With me</button>
+                </div>
+                <div class="card-item swiper-slide">
+                    <img class="user-image" src="assets/images_03.jpg" alt="Jack">
+                    <h2 class="user-name">Mr. Rakin</h2>
+                    <p class="user-profession">SQA Engineer</p>
+                    <button class="message-button">Contact With me</button>
+                </div>
+                <div class="card-item swiper-slide">
+                    <img class="user-image" src="assets/images_04.jpg" alt="Jack">
+                    <h2 class="user-name">Rose</h2>
+                    <p class="user-profession">Project Manager</p>
+                    <button class="message-button">Contact With me</button>
+                </div>
+                <div class="card-item swiper-slide">
+                    <img class="user-image" src="assets/images_05.jpg" alt="Jack">
+                    <h2 class="user-name">Mr. Akibul</h2>
+                    <p class="user-profession">Software Engineer</p>
+                    <button class="message-button">Contact With me</button>
+                </div>
+            </div>
+            <div class="swiper-pagination"></div>
+            <div class="swiper-button swiper-button-prev"></div>
+            <div class="swiper-button swiper-button-next"></div>
+        </div>
+    </div>
+
+  </main>
 
   <script>
     function showSidebar(){
@@ -38,5 +83,8 @@
       sidebar.style.display = 'none'
     }
   </script>
+  <!-- swiper JS CDN script Link  -->
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+  <script src="swiper.js"></script>
 </body>
 </html>
